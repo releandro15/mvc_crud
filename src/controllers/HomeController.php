51c9363650e397d10e53d99392107d9ada506e66
Raphael Leandro <releandro15@gmail.com>
2020-05-15 23:19:08 -0400

    Criado  rotas para fotos

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,11 +2,20 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\models\Test;
 
 class HomeController extends Controller {
 
     public function index() {
         $this->render('home', ['nome' => 'Bonieky']);
+    }
+
+    public function fotos() {
+        $this->render('fotos');
+    }
+
+    public function foto($parametro) {
+        echo 'Foto numero: '.$parametro['id'];
     }
 
     public function sobre() {
--- src/routes.php
+++ src/routes.php
@@ -6,3 +6,5 @@ $router = new Router();
 $router->get('/', 'HomeController@index');
 $router->get('/sobre/{nome}', 'HomeController@sobreP');
 $router->get('/sobre', 'HomeController@sobre');
+$router->get('/fotos', 'HomeController@fotos');
+$router->get('/foto/{id}', 'HomeController@foto');
